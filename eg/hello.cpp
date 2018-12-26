b546a100939dfa508528bbabf787aaaa8e7dee80
Robert A Alfieri <balfieri@nvidia.com>
2018-12-26 14:47:48 -0500

    tighten up

--- eg/hello.cpp
+++ eg/hello.cpp
@@ -6,10 +6,9 @@ using std::cout;
 
 int main( int argc, const char * argv[] )
 {
-    val exe  = val::exe_path();
-    val args = val::list( argc-1, argv+1 );
     cout << "Hello, world!\n";
-    cout << "Full path to this executable: "        << exe << "\n";
-    cout << "Arguments passed to this executable: " << args << "\n";
+
+    cout << "Full path to this executable: "        << val::exe_path()             << "\n";
+    cout << "Arguments passed to this executable: " << val::list( argc-1, argv+1 ) << "\n";
     return 0;
 }
