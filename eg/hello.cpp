268d5a8cccfd9a7dd6599c8dbfd0ab76c77268e5
Robert A Alfieri <balfieri@nvidia.com>
2018-12-26 14:37:37 -0500

    tweak

--- eg/hello.cpp
+++ eg/hello.cpp
@@ -6,6 +6,7 @@ int main( int argc, const char * argv[] )
 {
     val exe  = val::exe_path();
     val args = val::list( argc-1, argv+1 );
+    cout << "Hello, world!\n";
     cout << "Full path to this executable: "        << exe << "\n";
     cout << "Arguments passed to this executable: " << args << "\n";
     return 0;
