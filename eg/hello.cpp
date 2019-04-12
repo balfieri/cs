1114223d9be3b1640977f392884ff3a42bbe8b2f
Robert A Alfieri <balfieri@nvidia.com>
2019-04-12 15:51:26 -0400

    tweaks

--- eg/hello.cpp
+++ eg/hello.cpp
@@ -6,7 +6,7 @@ using std::cout;
 
 int main( int argc, const char * argv[] )
 {
-    cout << "Hello, world!\n\n";
+    cout << "Hello, world!\n";
 
     cout << "Full path to this executable: "        << val::exe_path()       << "\n";
     cout << "Arguments passed to this executable: " << val( argc-1, argv+1 ) << "\n";
